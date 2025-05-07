7f4fca82c559dc12f4a1b063d8e6d9825731f886
Edwin Carreño <ecarrenolozano@gmail.com>
2025-05-07 13:29:59 +0200

    chores: update content based on pre-commit checks and corrections

--- scripts/generate_data_subset_individual.sh
+++ scripts/generate_data_subset_individual.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Check if the input file is provided
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Usage: $0 <input_file.csv> <output_file.csv> <number of samples>"
     exit 1
 fi
 
 input_file="$1"
 output_file="$2"
+n_samples="$3"
 
 # Get the header
 head -n 1 "$input_file" > "$output_file"
@@ -16,7 +17,7 @@ head -n 1 "$input_file" > "$output_file"
 #tail -n +2 "$input_file" | shuf | head -n 10000 >> "$output_file"
 
 # Take the first rows
-tail -n +2 "$input_file" | head -n 1000 >> "$output_file"
+tail -n +2 "$input_file" | head -n $n_samples >> "$output_file"
 
 
 echo "Random sample saved to $output_file"
