0102919046e961fba1c6c20c6b10d810c069c2c6
Matthieu NAJM <matthieu.najm@pasteur.fr>
2025-10-28 13:56:46 +0100

    feat: first implementation of isolating trasncriptional interactions from networks.
    Co-authored by: Marko Baric <marko.baric@pasteur.fr>

--- weave_knowledge_graph.py
+++ weave_knowledge_graph.py
@@ -75,8 +75,8 @@ ONTOWEAVER_MAPPING_FILES = {
     "complexes": "./omnipath_secondary_adapter/adapters/complexes.yaml",
     "enzyme_PTM": "./omnipath_secondary_adapter/adapters/enzymePTM.yaml",
     "intercell": "./omnipath_secondary_adapter/adapters/intercell.yaml",
-    # "networks": "./omnipath_secondary_adapter/adapters/transcriptional_networks.yaml",
-    "networks": "./omnipath_secondary_adapter/adapters/lncrna_post_transcriptional_networks.yaml",
+    "networks": "./omnipath_secondary_adapter/adapters/transcriptional_networks.yaml",
+    # "networks": "./omnipath_secondary_adapter/adapters/lncrna_post_transcriptional_networks.yaml",
     # "networks": "./omnipath_secondary_adapter/adapters/small_molecule_protein_networks.yaml",
 }
 
@@ -348,9 +348,9 @@ def filtering_data(resource_name: str, dataframe: pd.DataFrame) -> pd.DataFrame:
         dataframe = dataframe
 
     if resource_name == "networks":
-        # dataframe = dataframe
+        dataframe = dataframe
         # dataframe = dataframe.groupby("type").get_group("transcriptional")
-        dataframe = dataframe.groupby("type").get_group("lncrna_post_transcriptional")
+        # dataframe = dataframe.groupby("type").get_group("lncrna_post_transcriptional")
 
     return dataframe
 
